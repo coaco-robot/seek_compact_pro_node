2ac7382822b1738aa5dafba9f6e32416c56d3074
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-03-07 07:32:02 +0100

    Integrated LibSeek with our node

--- src/compact_pro.cpp
+++ src/compact_pro.cpp
@@ -15,18 +15,28 @@ int main(int argc, char** argv)
     image_transport::ImageTransport it(nh);
     image_transport::Publisher pub = it.advertise("seek/image", 1);
 
+    // Create a SeekThermalPro camera
+    LibSeek::SeekThermalPro seek("");
+    if(!seek.open()) {
+        ROS_ERROR("Unable to open SEEK Compact Pro camera");
+        return -1;
+    }
+
     // ROS helper class to run the loop at a given frequency (Hz)
     ros::Rate loop_rate(FREQ);
     while (nh.ok()) {
-
         // Read an image from the SEEK Compact PRO camera
-        cv::Mat image = cv::imread("/home/dylan/catkin_ws/src/libseek_thermal_driver/lena.jpeg", CV_LOAD_IMAGE_COLOR);
-        if(image.empty()) {
+        cv::Mat frame, grey_frame; // = cv::imread("/home/dylan/catkin_ws/src/libseek_thermal_driver/lena.jpeg", CV_LOAD_IMAGE_COLOR);
+        if(!seek.read(frame)) {
             ROS_ERROR("SEEK Compact Pro camera cannot be read");
-            exit(1);
+            return -2;
         }
+        cv::normalize(frame, grey_frame, 0, 65535, cv::NORM_MINMAX);
+
         // Convert the image to a readable format for OpenCV
-        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+        grey_frame.convertTo(grey_frame, CV_8UC1, 1.0/256.0);
+        cv::cvtColor(grey_frame, grey_frame, cv::COLOR_GRAY2BGR);
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", grey_frame).toImageMsg();
 
         // Publish message to the ROS network
         pub.publish(msg);
